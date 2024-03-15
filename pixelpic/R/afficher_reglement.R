c0207ce8015bb93d56570264e115a03f2acf52b6
kenewy diallo <khadidiatou-kenewy.diallo@etu.umontpellier.fr>
2024-03-15 22:29:22 +0100

    modif

---
+++ pixelpic/R/afficher_reglement.R
@@ -0,0 +1,7 @@
+#' Afficher les détails du jeu
+#'
+#' @return Les détails du jeu en texte
+#' @export
+afficher_reglement_text <- function() {
+  "Bienvenue dans le jeu Picross!\n\nRèglement:\n1. Remplissez les cellules pour révéler l'image cachée.\n2. Les nombres sur les côtés indiquent le nombre de cases consécutives à remplir sur chaque ligne et colonne.\n3. Cliquez une fois pour remplir une cellule, deux fois pour marquer avec un X, et à nouveau pour revenir à l'état initial."
+}
