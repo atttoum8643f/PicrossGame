5e8bd1e54ece3f1d283c138e9cb7df597a328532
kenewy diallo <khadidiatou-kenewy.diallo@etu.umontpellier.fr>
2024-03-15 22:31:01 +0100

    ajout de la barre

---
+++ pixelpic/R/afficher_barre_verification.R
@@ -0,0 +1,36 @@
+#' Afficher la barre de vérification
+#'
+#' Cette fonction génère une interface utilisateur Shiny pour afficher la barre de vérification
+#' avec des boutons interactifs pour sélectionner différents modes d'interaction avec la grille Picross.
+#'
+#' @return Une interface utilisateur Shiny pour la barre de vérification.
+#'
+#' @examples
+#' # Afficher la barre de vérification
+#' afficher_barre_verification()
+#'
+#' @export
+afficher_barre_verification <- function() {
+  divs_barre <- list(
+    tags$div(
+      style = "display: flex; margin-top: 20px;",
+      actionButton(
+        inputId = "btn_noir",
+        label = "Noir",
+        style = "width: 70px; background-color: black; color: white; margin-right: 5px;"
+      ),
+      actionButton(
+        inputId = "btn_croix_rouge",
+        label = icon("times", class = "fa-2x"),
+        style = "width: 60px; background-color: white; color: red; border: 1px solid red; margin-right: 5px;"
+      ),
+      actionButton(
+        inputId = "btn_verification",
+        label = "Vérification",
+        style = "width: 100px; background-color: green; color: white;"
+      )
+    )
+  )
+
+  do.call(tagList, divs_barre)
+}
